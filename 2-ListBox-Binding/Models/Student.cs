2d083b7c714447cf7ddf7c45cf044e9aa9e36eb5
GreedNeSS <ruslan199126@gmail.com>
2021-12-20 23:13:42 +0300

    ListBox

---
+++ 2-ListBox-Binding/Models/Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+
+namespace _2_ListBox_Binding.Models
+{
+    public class Student: INotifyPropertyChanged
+    {
+        public string Icon { get; set; }
+        public string FirstName { get; set; }
+        public int Completion { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            var handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
